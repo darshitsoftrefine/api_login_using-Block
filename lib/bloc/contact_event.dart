fabfbf54138b0704a3e507483b251d6dd394bfd3
Darshit Shah <darshit.softrefine@gmail.com>
2023-06-08 18:27:01 +0530

    Second Commit

---
+++ lib/bloc/contact_event.dart
@@ -0,0 +1,11 @@
+part of 'food_bloc.dart';
+
+abstract class FoodEvent extends Equatable {
+  const FoodEvent();
+}
+class FetchFoodEvent extends FoodEvent {
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+
+}
